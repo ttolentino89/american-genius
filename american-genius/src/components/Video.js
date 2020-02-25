379c3d7371a2dd77c5f5d90c58e27365e561dac9
ttolentino89 <trishatolentino89@gmail.com>
2020-02-25 18:28:29 -0500

    video component is now background

--- american-genius/src/components/Video.js
+++ american-genius/src/components/Video.js
@@ -5,26 +5,40 @@ class Video extends Component {
 
   render() {
      const opts = {
-       height: '390',
-       width: '640',
+       // height: '390',
+       // width: '640',
        playerVars: { // https://developers.google.com/youtube/player_parameters
-         autoplay: 1
+         autoplay: 1,
+         // controls: 0,
+         rel: 0,
+         showinfo: 0
        }
      };
 
      return (
+    <div className="video-background">
+      <div className="video-foreground">
        <YouTube
          videoId="egGUqv8LTkw"
          opts={opts}
          onReady={this._onReady}
+         className="video-iframe"
+          onEnd={this._onEnd}
        />
+      </div>
+    </div>
      );
    }
 
    _onReady(event) {
      // access to player in all event handlers via event.target
      event.target.pauseVideo();
+    event.target.mute();
    }
+
+    _onEnd(event) {
+     event.target.playVideo();
+    }
  }
 
 export default Video;
