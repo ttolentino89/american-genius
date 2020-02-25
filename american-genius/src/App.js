1df9859943314c110496baf791df683f4b59008e
ttolentino89 <trishatolentino89@gmail.com>
2020-02-25 16:23:34 -0500

    FINALLY GOT YOUTUBE TO WORK

--- american-genius/src/App.js
+++ american-genius/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Video from './components/Video'
 
 
 function App() {
   return (
     <div className="App">
     <h1>This is America</h1>
+    <Video />
     </div>
   );
 }
---
+++ american-genius/src/components/Video.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import YouTube from 'react-youtube';
+
+class Video extends Component {
+
+  render() {
+     const opts = {
+       height: '390',
+       width: '640',
+       playerVars: { // https://developers.google.com/youtube/player_parameters
+         autoplay: 1
+       }
+     };
+
+     return (
+       <YouTube
+         videoId="egGUqv8LTkw"
+         opts={opts}
+         onReady={this._onReady}
+       />
+     );
+   }
+
+   _onReady(event) {
+     // access to player in all event handlers via event.target
+     event.target.pauseVideo();
+   }
+ }
+
+export default Video;
