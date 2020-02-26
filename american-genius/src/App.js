995294e9a0119b1216fe2b1a05eb2de45b96e413
ttolentino89 <trishatolentino89@gmail.com>
2020-02-25 21:57:07 -0500

    deleted h1 from app

--- american-genius/src/App.js
+++ american-genius/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import Video from './components/Video'
 
 
 function App() {
   return (
     <div className="App">
-    <h1>This is America</h1>
-    <Video />
+    <div className="vid">
+  <iframe frameborder="0" allowfullscreen height="100%" width="100%"
+    src="https://youtube.com/embed/egGUqv8LTkw?autoplay=1&controls=0&rel=0&showinfo=0&autohide=1&mute=1">
+  </iframe>
+</div>
     </div>
   );
 }
