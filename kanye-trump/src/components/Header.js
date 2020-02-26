3c8517f38d6d9d9dbcc230de9aacf6c60b2d7fbb
ttolentino89 <trishatolentino89@gmail.com>
2020-02-26 16:31:48 -0500

    gatsby scaffolding

---
+++ kanye-trump/src/components/Header.js
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="content">
+      <div className="inner">
+        <h1>American Genius</h1>
+        <p>
+          A "conversation" with Kanye West and President Donald Trump
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Kanye Says <i class="fa fa-comment-o" aria-hidden="true"></i>
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Trump Thinks <i class="fa fa-commenting-o" aria-hidden="true"></i>
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
